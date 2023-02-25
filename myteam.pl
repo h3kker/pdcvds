22c7ed6bbfc4805db6c68ba8c777b2f46c0fdd77
Heinz Axelsson-Ekker <haxel.hekker@testha.se>
2023-02-25 01:00:02 +0100

    inital

---
+++ myteam.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/env perl
+
+use v5.36;
+
+use Path::Tiny;
+use Mojo::UserAgent;
+use Mojo::JSON qw(encode_json);
+
+my $ua = Mojo::UserAgent->new;
+
+my $pw = path('.pw')->slurp;
+chomp $pw;
+
+my $res = $ua->max_redirects(2)->post('https://www.pdcvds.com/savelogin.php' => form => { 
+    form => 'frmlogin', 
+    username => 'h3kker', 
+    password => $pw,
+  })->result;
+
+die 'Could not log in: '.$res->code
+  unless $res->is_success;
+
+$res = $ua->get('https://www.pdcvds.com/myteam.php?mw=1&y=2023')->result;
+die 'Could not fetch team: '.$res->code
+  unless $res->is_success;
+
+my %team_map = (
+  'Israel - Premier tech' => 'Israel - Premier Tech',
+  'EF Education - Easypost' => 'EF Education-EasyPost',
+  'Ag2r Citroën' => 'AG2R Citroën Team',
+  'Bahrain Victorious' => 'Bahrain - Victorious',
+  'Bora - Hansgrohe' => 'BORA - hansgrohe',
+  'Groupama - Fdj' => 'Groupama - FDJ',
+  'Ineos Grenadiers' => 'INEOS Grenadiers',
+  'Jumbo - Visma' => 'Jumbo-Visma',
+  'Uno-X Pro Cycling' => 'Uno-X Pro Cycling Team',
+);
+
+my @riders;
+my $page = $res->dom;
+my $rows = $page->at('div[id="content"] table')->children('tr');
+$rows->tail(-1)->head(-1)->each(sub($row, $n) {
+  my $cols = $row->children('td')->to_array;
+  my $team = $cols->[2]->at('a')->attr('title');
+  $team = $team_map{$team} || $team;
+  push @riders, {
+    country => $cols->[1]->at('img')->attr('alt'),
+    team_short => $cols->[2]->at('a')->text,
+    team => $team,
+    cat => $cols->[3]->all_text,
+    name => $cols->[4]->at('a')->text,
+    age => $cols->[5]->text,
+    price => $cols->[6]->at('a')->text,
+    previous => $cols->[7]->text,
+    score => $cols->[8]->text,
+  };
+});
+
+say encode_json(\@riders);
---
+++ race-info.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/env perl
+
+use v5.36;
+
+use Mojo::UserAgent;
+use Mojo::JSON qw(encode_json);
+use Path::Tiny;
+use Mojo::Util qw(slugify);
+use utf8;
+
+my $start_url = shift @ARGV ||
+  die "Usage: $0 [url]";
+
+my $ua = Mojo::UserAgent->new;
+
+my @subs = ('result/overview', 'gc/overview');
+my $ov_res;
+for my $sub (@subs) {
+  my $overview_url = $start_url;
+  $overview_url =~ s,startlist$,$sub,;
+  $ov_res = $ua->get($overview_url)->result;
+  die 'Unable to fetch: '.$ov_res->code
+    unless $ov_res->is_success;
+  
+  unless ($ov_res->dom->at('div.main h1')->text eq 'Page not found') {
+    last;
+  }
+}
+
+
+my $infos = $ov_res->dom->at('ul.infolist')->find('li')->to_array;
+my $start_date = $infos->[0]->find('div')->last->text;
+my $end_date = $infos->[1]->find('div')->last->text;
+
+my $res = $ua->get($start_url)->result;
+die 'Unable to fetch: '.$res->code
+  unless $res->is_success;
+
+my $race = $res->dom->at('div.main h1')->text;
+
+my $list = $res->dom->at('ul.startlist_v3')->find('li.team');
+
+my $riders = $list->map(sub($t) {
+  my $team = $t->at('b a')->text;
+  return $t->find('ul li')->map(sub($r) {
+    my $number = $r->text;
+    $number =~ tr/ //d;
+    my $name = $r->at('a span')->text;
+    $name =~ /^([\p{Upper}\-'ß ]+) (.+)$/;
+    die "$name does not match"
+      unless $1 && $2;
+    return {
+      name => sprintf("%s %s" => $2, join('', map { ucfirst(lc $_) } split "([ '-])", $1)),
+      team => $team,
+      number => $number,
+    }
+  });
+})->flatten;
+
+path("race-".$start_date."-".slugify($race).".json")->spew(
+  encode_json({
+    race => $race,
+    start_date => $start_date,
+    end_date => $end_date,
+    riders => $riders->TO_JSON
+  })
+);
