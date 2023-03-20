37da283279321263bb30c222ab0f322ac8869d58
Heinz Axelsson-Ekker <haxel.hekker@testha.se>
2023-03-20 09:15:53 +0100

    start pdcvds result fetching

--- lib/PdcVds.pm
+++ lib/PdcVds.pm
@@ -97,7 +97,7 @@ sub login($self) {
     $self->is_logged_in(1);
 }
 
-sub _map_name($self, $name) {
+sub _map_name($name) {
     state %name_map = (
         'Sam Watson' => 'Samuel Watson',
         'Mattias Skjelmose Jensen' => 'Mattias Skjelmose',
@@ -110,7 +110,7 @@ sub _map_name($self, $name) {
     $name_map{$name} || $name;
 }
 
-sub _map_team($self, $team) {
+sub _map_team($team) {
     state %team_map = (
         'Israel - Premier tech' => 'Israel - Premier Tech',
         'EF Education - Easypost' => 'EF Education-EasyPost',
@@ -147,7 +147,7 @@ sub get_riders($self) {
     $rows->tail(-1)->head(-1)->each(sub($row, $n) {
         my $cols = $row->children('td')->to_array;
         my $team = $cols->[2]->at('a')->attr('title');
-        $team = $self->_map_team($team);
+        $team = _map_team($team);
         my $rider_url = Mojo::URL->new($cols->[4]->at('a')->attr('href'));
         my $rider_id = $rider_url->query->param('pid');
         my $base_info = {
@@ -156,7 +156,7 @@ sub get_riders($self) {
             team_short => $cols->[2]->at('a')->text,
             team => $team,
             cat => $cols->[3]->all_text,
-            name => $self->_map_name($cols->[4]->at('a')->text),
+            name => _map_name($cols->[4]->at('a')->text),
             age => $cols->[5]->text,
             price => $cols->[6]->at('a')->text+0,
             previous => $cols->[7]->text+0,
@@ -212,7 +212,7 @@ sub get_rider_info($self, $pid, $year=$self->year) {
                 }
                 elsif ($cols->[0]->text eq 'UCI Team' && $cols->[1]->at('a')->text) {
                     my ($team, $team_short) = ($cols->[1]->at('a')->text =~ /(.*) \((\w+)\)/);
-                    $info->{team} = $self->_map_team($team)
+                    $info->{team} = _map_team($team)
                         if $team;
                     $info->{team_short} = $team_short
                         if $team_short;
@@ -280,6 +280,144 @@ sub get_position($self) {
     push $self->current_team->{standings}->@*,
         { date => DateTime->now->iso8601, position => $cur_pos};
     $cur_pos;
+}
+
+sub results_list($self) {
+    my $res = $self->ua->get($self->base_url.'/results.php?mw=1&y='.$self->year)->result;
+    die 'Could not fetch results: '.$res->code
+        unless $res->is_success;
+    $res->dom->at('div#content table.noevents')->children('tr')->tail(-1)->head(-1)->map(sub ($row) {
+        my $tds = $row->find('td')->to_array;
+        my $link = $tds->[4]->at('a')->attr('href');
+        my ($id) = ($link =~ m/event=(\d+)/);
+        return {
+            name => $tds->[4]->at('a')->text,
+            link => $self->base_url.'/results.php'.$link,
+            id => $id,
+        }
+    })->to_array;
+}
+
+sub _parse_race_date($date_str) {
+    my $date_parser = DateTime::Format::Strptime->new(
+        pattern => '%d-%b-%Y',
+        on_error => 'croak',
+    );
+
+    if ($date_str =~ /, (\w+) (\d+), (\d+)/) {
+        return $date_parser->parse_datetime(sprintf "%d-%s-%d" => $2, $1, $3)->ymd('-');
+    }
+    else {
+        die 'Unable to parse date: '.$date_str->text;
+    }
+
+}
+
+sub race_info($self, $race_info) {
+    my $res = $self->ua->get($race_info->{link})->result;
+    die 'Could not fetch race '.$race_info->{link}.': '.$res->code
+        unless $res->is_success;
+
+    $res->dom->find('h2')->first(sub($e) { $e->text =~ /^Results/ })
+        ->following('table.noevents')->first->find('tr')->tail(-1)->each(sub ($row, $n) {
+            my $tds = $row->find('td')->to_array;
+            if ($tds->[0]->text eq 'Type') {
+                $race_info->{type} = $tds->[1]->text;
+            }
+            elsif ($tds->[0]->text eq 'Date' || $tds->[0]->text eq 'First stage') {
+                $race_info->{start_date} = _parse_race_date($tds->[1]->text);
+            }
+            elsif ($tds->[0]->text eq 'Last stage') {
+                $race_info->{end_date} = _parse_race_date($tds->[1]->text);
+            }
+        });
+    $race_info->{results} = {};
+    my $parse_result_row = sub($row, $type) {
+        my $tds = $row->find('td')->to_array;
+        my $pos = $tds->[0]->text;
+        $pos =~ tr/\. //d;
+        return {
+            rank => $pos+0,
+            name => _map_name($tds->[3]->at('a')->text),
+            points => $tds->[4]->text+0,
+            type => $type,
+        };
+    };
+    
+    if ($race_info->{type} eq 'Single-day race') {
+        $race_info->{results} = { final => $res->dom->find('h3')->first(sub($e) { $e->text eq 'Results'})
+            ->following('table.noevents')->first->find('tr')->tail(-1)->head(-1)->map(sub ($row) {
+                $parse_result_row->($row, 'oneday');
+            })->to_array };
+        return $race_info;
+    }
+    else {
+        my $head = $res->dom->find('h3')->first(sub($e) { $e->text =~ /^Stage/ });
+        $race_info->{results} = {};
+        unless ($head) {
+            # must be overview page
+            my $stage_table = $res->dom->find('table.noevents')->to_array->[1];
+            $race_info->{results}{stages} = [];
+            $stage_table->find('tr')->tail(-1)->each(sub ($row, $n) {
+                my $tds = $row->find('td')->to_array;
+                my $stage_link = $tds->[1]->at('a');
+                return unless $stage_link;
+
+                my $date = _parse_race_date($tds->[0]->text.', '.$self->year);
+                my ($num, $stage_name) = ($stage_link->text =~ /^(\d+)\. (.+)/);
+                my $link = $self->base_url.'/results.php'.$stage_link->attr('href');
+                my $results = $self->race_info({ link => $link });
+                push $race_info->{results}{stages}->@*, {
+                    stage_date => $date,
+                    stage => $num,
+                    name => $stage_name,
+                    gc => $results->{gc},
+                    result => $results->{result},
+                    jerseys => $results->{jersey},
+                };
+            });
+            $race_info->{results}{final} = $race_info->{results}{stages}[-1]{gc};
+            $race_info->{results}{final_jerseys} = $race_info->{results}{stages}[-1]{jerseys};
+            return $race_info;
+        }
+
+        my $state;
+        my $results = {
+            result => [],
+            jersey => [],
+            gc => [],
+        };
+        $head->following('table.noevents')->first->find('tr')->each(sub($row, $n) {
+            my $heads = $row->find('th')->to_array;
+            if (scalar $heads->@* == 2) {
+                if ($heads->[1]->text =~ /^Placing/) {
+                    $state = 'result';
+                }
+                elsif ($heads->[1]->text =~ /^Intermediate/) {
+                    $state = 'jersey';
+                }
+                elsif ($heads->[1]->text =~ /Final leader/) {
+                    $state = 'gc';
+                }
+                elsif ($heads->[1]->text =~ /Final .* jersey/) {
+                    $state = 'jersey';
+                }
+                else {
+                    die 'Unexpected: '.$heads->[1]->text.' on '.$race_info->{link};
+                }
+                return;
+            }
+            return if (($row->attr('class')//'') eq 'lite') ||
+                $row->find('td')->size == 1;
+            
+            push $results->{$state}->@*, $parse_result_row->($row, $state);
+        });
+        return $results;
+    }
+    
+
+    
+
 }
 
 sub write_team($self) {
---
+++ vds-results.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+use lib qw(./lib);
+
+use v5.36;
+
+use ProcyclingStats;
+use Getopt::Long;
+use PdcVds;
+
+my $year = 2023;
+my $race_links = [];
+GetOptions(
+    'race=s@' => \$race_links,
+    'year=i' => \$year,
+) or die("Usage!");
+
+use Mojo::File;
+use Mojo::Collection;
+use Mojo::JSON qw(encode_json);
+
+my $pdc = PdcVds->new(year => 2023);
+
+my $races;
+if (scalar $race_links->@*) {
+    $races = [ map { { link => $_ } } $race_links->@* ];
+}
+else {
+    $races = $pdc->results_list;
+}
+
+for my $race ($races->@*) {
+    my $rr = $pdc->race_info($race);
+    use Data::Dumper; say Dumper $rr;
+}
