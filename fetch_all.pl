73826ce3ef615510d6b5dd351a8533975650c932
Heinz Axelsson-Ekker <haxel.hekker@testha.se>
2024-03-29 20:52:49 +0100

    initisl sync, wip

---
+++ fetch_all.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+use lib qw(./lib);
+
+use v5.36;
+
+binmode(STDOUT, ":utf8");
+
+use PdcVds;
+use ProcyclingStats;
+use DateTime;
+use Getopt::Long;
+my $year = DateTime->now->year;
+GetOptions(
+    'year=i' => \$year)
+    or die("Usage!");
+
+my $pcs = ProcyclingStats->new;
+my $pdc = PdcVds->new(year => $year);
+$pdc->get_teams;
