d4e8182278ff34a39a2da4f4052c18cbc81edeb1
Heinz Axelsson-Ekker <haxel.hekker@testha.se>
2024-11-09 23:38:28 +0100

    reproduce old json bases stats

---
+++ site/load.R
@@ -0,0 +1,40 @@
+
+library(dplyr)
+library(lubridate)
+library(tidyr)
+cur_year <- 2024
+
+pdc_db <- DBI::dbConnect(RSQLite::SQLite(), "../pdcvds.db")
+riders <- as_tibble(tbl(pdc_db, "riders")) %>%
+  mutate(dob=ymd(dob), born=year(dob))
+teams <- as_tibble(tbl(pdc_db, "teams")) %>% mutate(mine=mine==1)
+team_riders <- as_tibble(tbl(pdc_db, "team_riders"))
+races <- as_tibble(tbl(pdc_db, "races")) %>%
+  mutate(start_date=ymd(start_date), end_date = ymd(end_date))
+stages <- as_tibble(tbl(pdc_db, "stages")) %>%
+  mutate(date = ymd(date))
+race_results <- as_tibble(tbl(pdc_db, "race_results"))
+riders_seen <- as_tibble(tbl(pdc_db, "riders_seen"))
+riders_prices <- as_tibble(tbl(pdc_db, "rider_prices"))
+
+cur_riders <- riders %>%
+  inner_join(riders_seen %>% filter(year == cur_year))
+
+
+cur_year <- year(now())
+my_team <- teams %>% filter(year == cur_year, mine) %>% pull(uid)
+
+my_riders <- teams %>%
+  filter(year == cur_year & mine) %>%
+  select(-name) %>%
+  inner_join(team_riders) %>%
+  inner_join(riders, by = join_by(pid)) %>%
+  inner_join(riders_prices) %>%
+  select(-mine, -starts_with("spec_"))
+
+# add result date from stage/race
+race_results <- race_results %>%
+  left_join(stages) %>%
+  inner_join(races, by=join_by(event_id, year)) %>%
+  mutate(date = if_else(is.na(date), start_date, date)) %>%
+  select(type = type.x, pos, pid, points, event_id, stage_id, year, date)
